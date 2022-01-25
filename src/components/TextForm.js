d5b3229bc87609dcce4888122c852ff516ff081f
Manali Jain <manalijain346@gmail.com>
2022-01-25 22:42:49 +0530

    initial commit

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -16,7 +16,7 @@ export default function TextForm(props) {
     <div>
         <h1>{props.heading}</h1>
         <div className="mb-3">
-            <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+            <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8" placeholder="Enter text here"></textarea>
         </div>
         <button className="btn btn-primary" onClick={handleUpClick}>Convert to Uppercase</button>
     </div>
