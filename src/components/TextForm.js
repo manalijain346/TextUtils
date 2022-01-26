29d55240b37b80bce8920ba7c1186dd6b76a2ece
Manali Jain <manalijain346@gmail.com>
2022-01-26 23:11:54 +0530

    added more functionalities

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -2,7 +2,8 @@ import React,{useState} from 'react'
 
 export default function TextForm(props) {
 
-  const [text,setText]=useState()
+  const [text,setText]=useState("")
+  const trimText=text.trim();
 
   const handleUpClick=()=>{
       let newText=text.toUpperCase();
@@ -12,13 +13,46 @@ export default function TextForm(props) {
   const handleOnChange=(event)=>{
       setText(event.target.value)
   }
+
+  const handleLoClick=()=>{
+    let newText=text.toLowerCase();
+    setText(newText)
+
+  }
+
+  const handleClearClick=()=>{
+    let clearText="";
+    setText(clearText)
+
+  }
+
+  const handleReverse = ()=>{
+    let reversText = text.split('').reverse().join("")
+    setText(reversText)
+  }
+
   return (
-    <div>
+    <>
+    <div className='container'>
         <h1>{props.heading}</h1>
         <div className="mb-3">
             <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8" placeholder="Enter text here"></textarea>
         </div>
-        <button className="btn btn-primary" onClick={handleUpClick}>Convert to Uppercase</button>
+        <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to Uppercase</button>
+        <button className="btn btn-primary mx-2" onClick={handleLoClick}>Convert to LowerCase</button>
+        <button className="btn btn-primary mx-2" onClick={handleClearClick}>Clear Text</button>
+        <button className="btn btn-primary mx-2" onClick={handleReverse}>Reverse</button>
+
+
     </div>
+    <div className="container my-3">
+      <h2>Your text summary</h2>
+      <p>{trimText.split(" ").length} words and {text.length} characters.</p>
+      <p>{0.008 * text.split("").length} Minutes read</p>
+      <h2>Preview</h2>
+      <p>{text}</p>
+
+    </div>
+    </>
   )
 }
